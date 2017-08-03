76fc23708ac8c01399bbad2d01f7c9b18abf6a4e
靳小健 <228322991@qq.com>
2017-08-03 16:12:44 +0800

    up

--- 08/vue03/main.js
+++ 08/vue03/main.js
@@ -1,82 +1,124 @@
-function Event() {
-    this.events = {};
+"use strict"
+/* 此文件为一些过渡性代码 */
+/*use ES5 defineProperty*/
+function Observer (data) {
+  this.data = data;
+  this.dep = new Dep();
+
+  if(Array.isArray(data)){
+    //暂不考虑数组
+  }else{
+    this.makeObserver(data);
+  }
 }
-Event.prototype = {
-    on: function(attr, callback) {
-        if (this.events[attr]) {
-            this.events[attr].push(callback);
-        } else {
-            this.events[attr] = [callback];
+
+Observer.prototype.setterAndGetter = function (key, val) {
+  let dep = new Dep();
+  if(typeof val === 'object'){
+    var childOb = new Observer(val);
+  }
+
+  Object.defineProperty(this.data, key, {
+    enumerable: true,
+    configurable: true,
+    get: function(){
+      console.log('你访问了' + key);
+
+      if(Dep.target){
+        dep.depend();
+        if(childOb){
+          childOb.dep.depend();
         }
+      }
+      return val;
     },
+    set: function(newVal){
+      console.log('你设置了' + key);
+      console.log('新的' + key + '=' + newVal);
 
-    off: function(attr) {
-        for (let key in this.events) {
-            if (this.events.hasOwnProperty(key) && key === attr) {
-                delete this.events[key];
-            }
-        }
-    },
-    trigger: function(attr, ...arg) {
-        this.events[attr] && this.events[attr].forEach(function(item) {
-            item(...arg);
-        })
+      if(newVal === val) return
+      val = newVal;
+
+      if(typeof val === 'object'){
+        childOb = new Observer(newVal);
+      }
+
+      dep.notify();
     }
+  })
 }
-
-
-
-
-
-function Vue(data) {
-    this.data = data;
-    this.walk(data);
-    this.watch = new Event();
-}
-
-let p = Vue.prototype;
-
-p.$watch = function(attr, callback) {
-    this.watch.on(attr, callback);
-}
-
-p.walk = function(obj) {
-    let val;
-    for (let key in obj) {
-
-        if (obj.hasOwnProperty(key)) {
-            val = obj[key];
-
-            if (typeof val === 'object') {
-                new Vue(val);
-            }
-
-            this.convert(key, val);
-        }
+Observer.prototype.makeObserver = function (obj) {
+  let val;
+  for(let key in obj){
+    if(obj.hasOwnProperty(key)){
+      val = obj[key];
+      //深度遍历
+      if(typeof val === 'object'){
+        new Observer(val);
+      }
     }
-};
+    this.setterAndGetter(key, val);
+  }
+}
+Observer.prototype.$watch = function(attr, callback){
+  //this.eventsBus.on(attr, callback);
+  for(let key in this.data){
+    if(this.data.hasOwnProperty(key) && typeof this.data[key] === 'object'){
+      this.data[key].__ob__.eventsBus.on(attr, callback);
+    }
+  }
+}
 
-p.convert = function(key, val) {
-    let _this = this;
-    Object.defineProperty(this.data, key, {
-        enumerable: true,
-        configurable: true,
-        get: function() {
-            alert('你访问了' + key);
-            return val;
-        },
-        set: function(newVal) {
+//观察者
+function Dep(){
+  this.subs = [];
+}
+Dep.target = null;
 
-            if (typeof newVal === 'object') {
-                new Vue(newVal);
-            }
-            _this.watch.trigger(key, val, newVal);
-            alert(`你设置了 ${key}, 新的值为${newVal}`);
-            val = newVal;
+Dep.prototype.depend = function(){
+  Dep.target.addDep(this);
+}
 
-        }
-    })
-};
+Dep.prototype.addSub = function(sub){
+  this.subs.push(sub);
+}
+Dep.prototype.notify = function(){
+  for(let i = 0, len = this.subs.length; i < len; i++ ){
+    this.subs[i].update();
+  }
+}
+
+
+//watcher
+function Watcher(value, attr){
+  this.value = value;
+  this.attr = attr;
+  this.get();
+}
+
+Watcher.prototype.beforeGet = function(){
+  Dep.target = this;
+}
+
+Watcher.prototype.get = function(){
+  this.beforeGet();
+
+  let val = this.value[this.attr];
+
+  if(typeof val === 'object'){
+    for(let childAttr in val){
+      new Watcher(val[childAttr], childAttr);
+    }
+  }
+}
+
+Watcher.prototype.addDep = function(dep){
+  dep.addSub(this);
+  console.log("我订阅了basicInfo的变化")
+}
+Watcher.prototype.update = function(){
+  console.log('name或者age变了，导致basicInfo变了，但是我要根据数据的变化来做一些牛逼的事情');
+}
 
 
 
