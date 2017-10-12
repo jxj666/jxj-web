780ec31be9204cff039ca91db42302846ff2378c
靳小健 <228322991@qq.com>
2017-10-12 16:29:52 +0800

    up

--- 10/1009.js
+++ 10/1009.js
@@ -1,5 +1,5 @@
-1单选(2分)
-以下代码执行完成后president.name的值是
+// 1单选(2分)
+// 以下代码执行完成后president.name的值是
 function setName(obj){
     obj.name = "obama";
     obj = {name:"clinton"};
@@ -7,190 +7,190 @@ function setName(obj){
 var president = {name:"bush"};
 setName(president);
 
-得分/总分
-
-A.
-"bush"
-
-0.00/2.00
-
-B.
-undefined
 
 
-C.
-null
+// A.
+// "bush"
 
 
-D.
-"obama"
 
-正确答案：D你错选为A
-2单选(2分)
-以下代码执行后a、b的值分别是
+// B.
+// undefined
+
+
+// C.
+// null
+
+
+// D.对
+// "obama"
+
+
+// 2单选(2分)
+// 以下代码执行后a、b的值分别是
 var str = "welcome to NetEase!welcome to MOOC!";
 var a = str.indexOf("to");
 var b = str.indexOf("to", a+1);
 
-得分/总分
-
-A.
-27, 8
 
 
-B.
-8, 27
-
-2.00/2.00
-
-C.
-8, -1
+// A.
+// 27, 8
 
 
-D.
--1,27
+// B.对
+// 8, 27
 
-正确答案：B你选对了
-3单选(2分)
-以下代码执行后a,b的值分别是
+
+
+// C.
+// 8, -1
+
+
+// D.
+// -1,27
+
+
+// 3单选(2分)
+// 以下代码执行后a,b的值分别是
 var a = 0;
 var b = false&&a++;
 
-得分/总分
 
-A.
-1，1
 
-B.
-1，0
+// A.
+// 1，1
 
-C.
-0，false
-2.00/2.00
+// B.
+// 1，0
 
-D.
-0，1
-正确答案：C你选对了
-4单选(2分)
-以下表达式返回结果分别是
+// C.对
+// 0，false
+
+
+// D.
+// 0，1
+
+// 4单选(2分)
+// 以下表达式返回结果分别是
 1&&0
 
-得分/总分
 
-A.
-0
-2.00/2.00
 
-B.
-NaN
+// A.对
+// 0
 
-C.
-false
 
-D.
-1
-正确答案：A你选对了
-5单选(2分)
-以下代码执行后，arr和result的值分别是
+// B.
+// NaN
+
+// C.
+// false
+
+// D.
+// 1
+
+// 5单选(2分)
+// 以下代码执行后，arr和result的值分别是
 var arr = [1,2,3]; 
 var result = arr.shift(); 
 
-得分/总分
 
-A.
-[1,2,3] 和 1
 
-B.
-[1,2,3] 和 3
+// A.
+// [1,2,3] 和 1
 
-C.
-[2,3] 和 1
-2.00/2.00
+// B.
+// [1,2,3] 和 3
 
-D.
-[2,3] 和 3
-正确答案：C你选对了
-6单选(2分)
-以下不是Number类型的是
-得分/总分
+// C.对
+// [2,3] 和 1
 
-A.
-"4"
-2.00/2.00
 
-B.
-0x9F
+// D.
+// [2,3] 和 3
 
-C.
-10
+// 6单选(2分)
+// 以下不是Number类型的是
 
-D.
-056
-正确答案：A你选对了
-7单选(2分)
-以下代码执行后,total的值为
+
+// A.对
+// "4"
+
+
+// B.
+// 0x9F
+
+// C.
+// 10
+
+// D.
+// 056
+
+// 7单选(2分)
+// 以下代码执行后,total的值为
 var total = 0,
     i = 5;
 do{
   total += i++;
 }while(i < 7)
-得分/总分
 
-A.
-6
 
-B.
-11
-2.00/2.00
+// A.
+// 6
 
-C.
-7
+// B.对
+// 11
 
-D.
-0
-正确答案：B你选对了
-8单选(2分)
-以下代码执行后a的值为:
+
+// C.
+// 7
+
+// D.
+// 0
+
+// 8单选(2分)
+// 以下代码执行后a的值为:
 var a = 10;
 function fn(x){
 	return --x;
 }
 fn(a);
-得分/总分
 
-A.
-8
 
-B.
-10
+// A.
+// 8
 
-C.
-11
+// B.对
+// 10
 
-D.
-undefined
-0.00/2.00
-正确答案：B你错选为D
-9单选(2分)
-以下代码执行后a的值为:
+// C.
+// 11
+
+// D.
+// undefined
+
+
+// 9单选(2分)
+// 以下代码执行后a的值为:
 var a = JSON.stringify({name: "jerry", age: 1, nick: undefined, tags: null});
-得分/总分
 
-A.
-'{"name":"jerry","age":1 }'
 
-B.
-'{"name":"jerry","age":1,"nick":undefined,"tags":null }'
+// A.
+// '{"name":"jerry","age":1 }'
 
-C.
-'{"name":"jerry","age":1,"tags":null}'
-2.00/2.00
+// B.
+// '{"name":"jerry","age":1,"nick":undefined,"tags":null }'
 
-D.
-'{"name":"jerry","age":"1","nick":"undefined","tags":"null" }'
-正确答案：C你选对了
-10单选(2分)
-以下代码执行后circle的值为:
+// C. 对
+// '{"name":"jerry","age":1,"tags":null}'
+
+
+// D.
+// '{"name":"jerry","age":"1","nick":"undefined","tags":"null" }'
+
+// 10单选(2分)
+// 以下代码执行后circle的值为:
 var circle = {x: 1, y: 0, r: 5};
 function move(shape, stepX, stepY){
 	shape.x = shape.x + stepX;
@@ -198,73 +198,70 @@ function move(shape, stepX, stepY){
 	return shape
 }
 move(circle, -2, 3);
-得分/总分
 
-A.
-{x: 1, y: 3, r: 5}
 
-B.
-{x: 4, y: -2, r: 5}
+// A.
+// {x: 1, y: 3, r: 5}
 
-C.
-{x: -1, y: 3, r: 5}
-2.00/2.00
+// B.
+// {x: 4, y: -2, r: 5}
 
-D.
-{x: 1, y: -3, r: 5}
-正确答案：C你选对了
-11多选(4分)
-以下表达式中返回结果是true的有
-得分/总分
+// C.对
+// {x: -1, y: 3, r: 5}
 
-A.
-"123"== 123
-1.33/4.00
 
-B.
-new String("123") == new String("123")
+// D.
+// {x: 1, y: -3, r: 5}
 
-C.
-new String("123") == "123"
-1.33/4.00
+// 11多选(4分)
+// 以下表达式中返回结果是true的有
 
-D.
-false == "0"
-1.33/4.00
-正确答案：A、C、D你选对了
-12填空(2分)
+
+// A.对
+// "123"== 123
+
+
+// B.
+// new String("123") == new String("123")
+
+// C.对
+// new String("123") == "123"
+
+
+// D.对
+// false == "0"
+
+
+// 12填空(2分)
 var obj = Object.create({a:1});
 obj.hasOwnProperty('a')返回结果是___
 
 
-得分/总分
+//  false
 
-true
- 0.00/2.00
-正确答案：false
-13填空(2分)
-以下代码实现了让数值x保留两位小数，请补全代码：
+
+// 13填空(2分)
+// 以下代码实现了让数值x保留两位小数，请补全代码：
 var x = 3.14159;
 x = Number(x.____________(2));
-得分/总分
 
-ceil
- 0.00/2.00
-正确答案：toFixed
-14填空(2分)
+
+// toFixed
+
+
+// 14填空(2分)
 new Date(2015, 10, 0).getDate() 的值为_____________
-得分/总分
 
-30
- 0.00/2.00
-正确答案：31
-15填空(2分)
-以下代码执行后:
+
+// 31
+
+
+// 15填空(2分)
+// 以下代码执行后:
 var ms = "share.jpeg".match(/^(?:[^.]+)\.(png|gif|jpg|jpeg)/);
 ms[1]的值为"______"
 
-得分/总分
 
--1
- 0.00/2.00
-正确答案：jpeg
+
+// jpeg
+
