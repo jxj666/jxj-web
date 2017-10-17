3f2164ec23ee02ad570f5e01d8410f4c3ed48116
靳小健 <228322991@qq.com>
2017-10-17 20:09:08 +0800

    up

---
+++ python_a/002.py
@@ -0,0 +1,5 @@
+str1=input('请输入一个人的名字')
+str2=input('请输入一个国家')
+print('{}想去{}'.format(str1,str2))
+
+ 
---
+++ python_a/003.py
@@ -0,0 +1,5 @@
+n=input('请输入整数 N:')
+sum=0
+for i in range(int(n)):
+	sum+=i+1
+print('1到N求和结果:',sum)
