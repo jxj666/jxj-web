74c5456d2934535d1e52b9c785c5cd4e296925de
靳小健 <228322991@qq.com>
2017-11-13 00:08:17 +0800

    up

---
+++ javascript_a/003.js
@@ -0,0 +1,17 @@
+// promise
+// pending fulfilled rejected
+// resolved 
+
+const promise = new Promise(function(resolve,reject){
+	if(){
+		resolve(value);
+	}else{
+		reject(error);
+	}
+})
+
+promise.then(function(value){
+
+},function(error){
+	
+})
