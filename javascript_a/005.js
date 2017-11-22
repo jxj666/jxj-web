0533c142c001cdf0c03b1f3b342e80973ae4bab2
靳小健 <228322991@qq.com>
2017-11-23 01:13:15 +0800

    up

--- javascript_a/005.js
+++ javascript_a/005.js
@@ -7,3 +7,34 @@ const promise = new Promise(function(resolve, reject) {
         reject(error);
     }
 });
+
+promise.then(function(value) {
+
+}, function(error) {
+
+});
+
+
+//promise 实例
+
+function timeout(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms, 'done');
+    });
+}
+timeout(5000).then((value) => {
+    console.log(value);
+})
+
+//promise 实例
+
+let promise = new Promise(function(resolve, reject) {
+    console.log('promise');
+    resolve();
+});
+promise.then(function() {
+    console.log('resolve');
+});
+console.log('hi');
+
+
