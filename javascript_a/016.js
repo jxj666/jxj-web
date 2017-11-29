70e490b84846de505600a752cf041a065ae57ae4
靳小健 <228322991@qq.com>
2017-11-29 23:54:25 +0800

    quick

---
+++ javascript_a/016.js
@@ -0,0 +1,28 @@
+// 快速排序
+
+//quick swap part
+
+function quick(arr) {
+    var left_arr = [];
+    var right_arr = [];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[0]) {
+            left_arr.push(arr[i]);
+        } else {
+            right_arr.push(arr[i]);
+        }
+    }
+    if (left_arr.length > 1) {
+        left_arr=quick(left_arr);
+    }
+    if (right_arr.length > 1) {
+        right_arr=quick(right_arr);
+    }
+    arr1 = left_arr.concat(arr[0], right_arr);
+    console.log(arr1);
+    return arr1;
+}
+
+
+var arr = [1, 11, 2, 22, 3, 33];
+quick(arr);
