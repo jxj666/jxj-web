dd29b6dbb3dda3d9cbdfed5ef0914baf3d3280ef
靳小健 <228322991@qq.com>
2017-12-02 21:30:46 +0800

    up

--- javascript_a/018.js
+++ javascript_a/018.js
@@ -2,13 +2,15 @@
 // charCodeAt unescape
 
 
-var to_unicode = function(str) {  
-    var res=[];  
-    for(var i=0;i < str.length;i++)  
-        res[i]=("00"+str.charCodeAt(i).toString(16)).slice(-4);  
-    return "\\u"+res.join("\\u");  
-}  
-var unicode_to = function(str) {  
-    str=str.replace(/\\/g,"%");  
-    return unescape(str);  
-}  
+function to_unicode(str) {
+    var res = [];
+    for (var i = 0; i < str.length; i++) {
+        res[i] = ('00' + str.charCodeAt(i).toString(16)).slice(-4);
+    }
+    return '\\u' + res.join('\\u');
+}
+
+function unicode_to(str) {
+    str.str.replace(/\\/g, '%');
+    return unescape(str);
+}
