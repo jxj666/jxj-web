406c19c91dd5594b14d87b690d40d0edb135e22c
靳小健 <228322991@qq.com>
2017-12-04 02:34:42 +0800

    vue

--- 11/vue04/vue.js
+++ 11/vue04/vue.js
@@ -1,11 +1,9 @@
-function Vue(obj) {
-    var el = obj.el;
-    var data = obj.data;
-    var dom = document.getElementById(el.slice(1));
+function Vue(data) {
+    var dom = document.querySelector(data.el);
     var html_old = dom.innerHTML;
     var html_new = html_old.replace(/{{(.*?)}}/g, function(word) {
         var arr = word.slice(2, -2).split('.');
-        return data[arr[0]][arr[1]];
+        return data.data[arr[0]][arr[1]];
     });
     dom.innerHTML = html_new;
 }
---
+++ 11/vue05/main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    user: {
+      name: 'youngwind',
+      age: 25
+    },
+    school: 'bupt',
+    major: 'computer'
+  }
+});
