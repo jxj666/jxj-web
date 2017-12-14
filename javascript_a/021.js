ac642f7845bb4f11ef57fe23c6c2315458f81736
靳小健 <228322991@qq.com>
2017-12-15 01:21:53 +0800

    cookie

---
+++ javascript_a/021.js
@@ -0,0 +1,17 @@
+// 函数getCookies()解析document.cookie， 并返回一个对象， 该对象的属性名为cookie的name，属性值为cookie的value。请写出该函数的实现代码
+//将cookie转换为js对象
+
+function getcookie() {
+    var cookie = {};
+    var all = document.cookie;
+    if (all) {
+        var list = all.split('; ');
+        for (var i = 0; i < list.length; i++) {
+            var arr = list[i].split('=');
+            var name = decodeURIComponent(arr[0]);
+            var value = decodeURIComponent(arr[1]);
+            cookie[name] = value;
+        }
+    }
+    return cookie;
+}
