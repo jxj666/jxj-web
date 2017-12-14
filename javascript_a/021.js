6dc2f1e95dd46283468279150d3b38390ef58a9b
靳小健 <228322991@qq.com>
2017-12-15 02:13:10 +0800

    style.opacity

--- javascript_a/021.js
+++ javascript_a/021.js
@@ -1,7 +1,7 @@
 // 函数getCookies()解析document.cookie， 并返回一个对象， 该对象的属性名为cookie的name，属性值为cookie的value。请写出该函数的实现代码
 //将cookie转换为js对象
 
-function getcookie() {
+function getCookie() {
     var cookie = {};
     var all = document.cookie;
     if (all) {
---
+++ javascript_a/022.js
@@ -0,0 +1,17 @@
+// 函数fadeout(element)实现了元素的淡出效果（即透明度从1变到0），动画时间为10秒钟。用定时器setInterval实现动画，写出函数fadeout的实现代码
+
+function fadeout(ele) {
+    var stepLength = 1 / 100;
+    if (!parseFloat(ele.style.opacity)) {
+        ele.style.opacity = 1;
+    }
+    function step() {
+        if (parseFloat(ele.style.opacity) - stepLength > 0) {
+            ele.style.opacity = parseFloat(ele.style.opacity) - stepLength;
+        } else {
+            ele.style.opacity = 0;
+            clearInterval(timer);
+        }
+    }
+    var timer = setInterval(step, 100);
+}
