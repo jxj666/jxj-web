9a8c5e14477eafaa5930f293ac22f264e0e8f15e
异阳 <228322991@qq.com>
2018-01-16 11:20:21 +0800

    up

--- javascript_a/029.js
+++ javascript_a/029.js
@@ -1,10 +1,26 @@
-const fs = require('fs');
+(function loop() {
+    console.log('应用继续')
+    setTimeout(function() {
+        loop()
+    }, 1000)
+})()
+let syncError = function() {
+    throw new Error('sync error')
+}
+let asyncError = function() {
+    throw new Error('async error')
+}
+try {
+    syncError()
+} catch (e) {
+    console.log('同步错误')
+}
+console.log('同步错误被捕获,应用继续')
+let fs = require('fs')
 process.on('uncaughtException', (err) => {
-  fs.writeSync(1, `Caught exception: ${err}\n`);
-});
-
+    fs.writeSync(1, `异步错误:${err}\n`)
+})
 setTimeout(() => {
-  throw new Error('Async Error');
-}, 500);
-
-console.log('run this line');
+    asyncError()
+}, 100)
+console.log('异步错误被捕获,应用继续')
