c37c20e6a5c02e986396fc9655e515b0a9839dfe
靳小健 <228322991@qq.com>
2018-05-11 15:00:38 +0800

    up

---
+++ html_a/10.js
@@ -0,0 +1,11 @@
+$(document).ready(main)
+function main(){
+$.ajax({
+	type:'post',
+	URL:'https://easy-mock.com/mock/5af53cd08efab658654cbd71/test/api/str',
+	success:str_html
+})	
+}
+function str_html(data){
+	console.log(data)
+}
