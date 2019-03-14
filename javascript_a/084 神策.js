817288791a31c8f3100da81f097a8ccd059c85c8
jxj322991 <228322991@qq.com>
2019-03-14 15:32:32 +0800

    fix

---
+++ javascript_a/084 神策.js
@@ -0,0 +1,31 @@
+/*
+ * @Description:
+ * @Author: 靳小健
+ * @Email: jinxiaojian@youxin.com
+ * @LastEditors: 靳小健
+ * @Date: 2018-08-15 11:34:44
+ * @LastEditTime: 2019-03-14 15:27:04
+ */
+var data = {
+  orderID: String(""),
+  orderJDID: String(""),
+  commodityID: String(""),
+  commodityJDSKU: String(""),
+  commodityName: String(""),
+  firstCommodity: String(""),
+  secondCommodity: String(""),
+  firstLevel: String(""),
+  secondLevel: String(""),
+  subitem: String(""),
+  commodityQuantity: Number(""),
+  originalPrice: Number(""),
+  presentPrice: Number(""),
+  isUseQuota: Boolean(""),
+  quotaAmount: Number(""),
+  quotaPeriod: Number(""),
+  isUseDownpayment: String(""),
+  downpaymentAmount: Number(""),
+  supplierName: String(""),
+  commodityDiscountAmount: Number(""),
+  totalPriceOfCommodity: Number(""),
+};
