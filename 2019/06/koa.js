5646b33167679780224c96849f404e2919ca61d9
jxj322991 <228322991@qq.com>
2019-06-11 10:33:54 +0800

    fix

--- 2019/06/koa.js
+++ 2019/06/koa.js
@@ -3,15 +3,13 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-06-10 19:09:40
- * @LastEditTime: 2019-06-10 19:16:53
+ * @LastEditTime: 2019-06-10 20:00:20
  * @LastEditors: 靳肖健
  */
 const Koa = require('koa');
+const fs = require('fs')
 const app = new Koa();
-let word = `
-<h1>Hello!</h1>
-<img src='http://jxjweb.top/fenhuan.jpg' width='100px'>
-`
+let word = fs.createReadStream('./test1.html')
 const main = ctx => {
   ctx.response.type = 'html';
   ctx.response.body = word;
--- 2019/06/node.js
+++ 2019/06/node.js
@@ -3,20 +3,49 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-06-10 17:35:09
- * @LastEditTime: 2019-06-10 19:03:10
+ * @LastEditTime: 2019-06-11 10:22:02
  * @LastEditors: 靳肖健
  */
 const http = require('http')
+const fs = require('fs')
 const hostname = '127.0.0.1'
 const port = 3000
-let word = `
-<h1>Hello!</h1>
-<img src='http://jxjweb.top/fenhuan.jpg' width='100px'>
-`
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end(word)
+  console.log(req.url)
+  if (req.url == '/') {
+    fs.readFile('index.html', function readData (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data)
+    })
+  } else if (req.url == '/get') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write("Hello World!")
+    res.end()
+  } else if (req.url == '/post') {
+    let content = {}
+    req.on('data', function (chunk) {
+      var arr = chunk.toString().split('&')
+      for (var i = 0; i < arr.length; i++) {
+        var arr1 = arr[i].split("=")
+        content[arr1[0]] = arr1[1]
+      }
+    })
+    req.on('end', function () {
+      res.writeHead(200, { "Content-Type": "text/plain" })
+      var res_obj = {
+        content: content,
+        success: 1
+      }
+      res.write(JSON.stringify(res_obj))
+      res.end()
+    })
+  } else {
+    fs.readFile('error.html', function readData (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data)
+    })
+  }
+
 })
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
