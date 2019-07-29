d542b33349d0f81fc3d4dc699112b7b461ec6309
jxj322991 <228322991@qq.com>
2019-07-29 16:22:43 +0800

    fix

--- js_2019/0729maxHeap.js
+++ js_2019/0729maxHeap.js
@@ -3,38 +3,40 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-07-29 10:52:16
- * @LastEditTime: 2019-07-29 15:24:13
+ * @LastEditTime: 2019-07-29 16:22:24
  * @LastEditors: 靳肖健
  */
 
 //因为声明的多个函数都需要数据长度,所以把len设置成为全局变量
 var len;
 
-//二叉树调整->建立大顶堆
+//二叉树调整->建立大顶堆,由下到上子树对比
 function buildMaxHeap (arr) {
-  len = arr.length;
-  var key = Math.floor(len / 2);
+  len = arr.length
+  var key = Math.floor(len / 2)
   for (var i = key; i >= 1; i--) {
-    heapify(arr, i);//堆调整
+    heapify(arr, i - 1)//堆调整
   }
 }
 
-//子树调整
+//子树节点对比
 function heapify (arr, i) {
-  var left = 2 * i, right = 2 * i + 1, father = i;
-  if (left <= len && arr[left - 1] > arr[father - 1]) { father = left; }
-  if (right <= len && arr[right - 1] > arr[father - 1]) { father = right; }
+  var left = 2 * i + 1, right = 2 * i + 2, father = i
+
+  if (left < len && arr[left] > arr[father]) { father = left }
+  if (right < len && arr[right] > arr[father]) { father = right }
+
   if (father != i) {
-    swap(arr, i - 1, father - 1);
+    swap(arr, i, father)
     // //二叉树整形为堆,排序可省略
-    // heapify(arr, father);
+    heapify(arr, father)
   }
 }
 
 //数组对调
 function swap (arr, i, j) {
   // es6数组对调
-  [arr[i], arr[j]] = [arr[j], arr[i]];
+  [arr[i], arr[j]] = [arr[j], arr[i]]
   //es3数组对调
   // arr[i] = [arr[j], arr[j] = arr[i]][0]
 }
@@ -49,5 +51,5 @@ function heapSort (arr) {
     // console.log(arr1)
     arr2.unshift(arr1.shift())
   }
-  return arr2;
+  return arr2
 }
