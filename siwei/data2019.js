4cc9ed8dca9a66337a8907f2dd1af7661a8a3380
jxj322991 <228322991@qq.com>
2019-12-09 00:03:02 +0800

    fix

---
+++ siwei/data2019.js
@@ -0,0 +1,91 @@
+/*
+ * @Description: 
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @Date: 2019-07-05 10:42:44
+ * @LastEditTime: 2019-12-08 23:56:39
+ * @LastEditors: 靳肖健
+ */
+var dataArr2019 = [
+  // { info:'', title: ``,text:``, img: ``,link:[] },
+
+  { info: '', title: `npm模块安装机制`, text: `npm 是 Node 的模块管理器,功能极其强大;它是 Node 获得成功的重要原因之一;正因为有了npm,我们只要一行命令,就能安装别人写好的模块 `, img: `19/09/npm模块安装机制.png`, link: ['http://www.ruanyifeng.com/blog/2016/01/npm-install.html', 'https://blog.csdn.net/qq_35430000/article/details/96425986'] },
+  { info: '', title: `js检测数据类型`, text: `Javascript 有两种数据类型,分别是基本数据类型(6种)和引用数据类型`, img: `19/09/js检测数据类型.png`, link: ['https://cloud.tencent.com/developer/article/1408414'] },
+  { info: 'spa', title: `SPA`, text: `单页面应用`, img: `19/09/SPA.png`, link: [] },
+
+
+  {
+    info: '', title: `sass`, text: `世界上最成熟、最稳定、最强大的专业级CSS扩展语言!`, img: `19/09/sass.png`, link: [
+      'https://www.cnblogs.com/hope666/p/6791790.html',
+      'https://www.sass.hk/docs/'
+    ]
+  },
+  {
+    info: '', title: `使用键的集合对象`, text: `Map/Set/WeakMap/WeakSet 均为js标准内置对象;用于储存特别结构数据这些集合对象在存储数据时会使用到键,支持按照插入顺序来迭代元素`, img: `19/09/使用键的集合对象.png`, link: [
+      'https://blog.csdn.net/qq_30101131/article/details/88544297',
+      'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects'
+    ]
+  },
+  {
+    info: '', title: `前端优先遍历`, text: `JavaScript深度优先遍历和广度优先遍历`, img: `19/09/前端优先遍历.png`, link: [
+      'https://blog.csdn.net/foradmin/article/details/89295707',
+      'https://note.youdao.com/ynoteshare1/index.html?id=be778364d9e1518ecb54afbe1e07a656&type=note'
+    ]
+  },
+
+
+
+  { info: 'nuxt', title: `NUXT`, text: `Nuxt.js 是一个基于 Vue.js 的通用应用框架;他预设了利用 Vue.js 开发服务端渲染的应用所需要的各种配置`, img: `19/09/NUXT.png`, link: ['https://zh.nuxtjs.org/', 'https://mp.weixin.qq.com/s?__biz=MjM5MTA1MjAxMQ==&mid=2651226973&idx=1&sn=2b540c1d7bb8e97379a9748f97797bac&chksm=bd495ad98a3ed3cf628a8670943d612487499c7a90a32cf861c282cbf22d4e408e5147c51b93&mpshare=1&scene=1&srcid=&sharer_sharetime=1567127900024&sharer_shareid=abea93acdfdbddf8c09d051b4b95d3ba&key=8bbaf831af1f1faeb8438d36d5ece1b7e02cebccffe0b13867f116d90b6ef3aa4483c545be76a432ea5ca1bed76cc004022d1966af7cf6825432be3d8ccc9e1907c8d57e7e1e46bd6dea4340792f8ced&ascene=1&uin=MzA4MTkwMDU0&devicetype=Windows+10&version=62060833&lang=zh_CN&pass_ticket=wnKhsmmvIv3Fftn6eGh48iKXtoQIjvKykG4P7dnWXeR96jpiQj82T92OPv%2Fqlb%2Fw'] },
+  { info: '', title: `vuex`, text: `Vuex 是一个专为 Vue.js 应用程序开发的状态管理模式;它采用集中式存储管理应用的所有组件的状态,并以相应的规则保证状态以一种可预测的方式发生变化`, img: `19/09/vuex.png`, link: ['https://vuex.vuejs.org/zh/'] },
+  { info: '', title: `严格模式`, text: `es5的严格模式是采用具有限制性JavaScript变体的一种方式`, img: `19/09/严格模式.png`, link: ['https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Strict_mode'] },
+
+
+  { info: '', title: `模型与视图`, text: `设计模式是对在某种环境中反复出现的问题以及解决该问题的方案的描述;mv*设计模式被独特的发展起来用于映射传统的输入、处理和输出功能在一个逻辑的图形化用户界面的结构中`, img: `19/08/模型与视图.png`, link: [] },
+  { info: '', title: `es6常用特性`, text: `es6标准于2015年6月正式发布了;它的目标,是使js可以用来编写复杂的大型应用程序,成为企业级开发语言`, img: `19/08/es6常用特性.png`, link: [] },
+  { info: '', title: `package`, text: `每个项目的根目录下面,一般都有一个package.json文件,定义了这个项目所需要的各种模块,以及项目的配置信息（比如名称、版本、许可证等元数据）`, img: `19/08/package.png`, link: ['http://javascript.ruanyifeng.com/nodejs/packagejson.html'] },
+
+
+  { info: '', title: `代码设计模式`, text: `并不是所有设计模式都是适合 JavaScript 的,我们需要注意一些比较常见的设计模式,比如工厂模式/单例模式/发布-订阅模式`, img: `19/08/代码设计模式.png`, link: [] },
+  { info: '', title: `this`, text: `this完全取决于函数调用的地方,而不是它被声明的地方`, img: `19/08/this.png`, link: [] },
+  { info: '', title: `闭包`, text: `当函数可以记住并访问所在的词法作用域时,就产生了闭包,即使函数是在当前词法作用域之外执行`, img: `19/08/闭包.png`, link: [] },
+  { info: '', title: `高阶函数`, text: `高阶函数就是输入参数里有函数,或者输出是函数的函数`, img: `19/08/高阶函数.png`, link: [] },
+
+
+
+  { info: 'node', title: `Node 异步`, text: `JavaScript 是单线程运行,异步操作特别重要;Node 的异步语法比浏览器更复杂`, img: `19/08/Node+异步.png`, link: ['http://www.ruanyifeng.com/blog/2018/02/node-event-loop.html'] },
+  { info: '', title: `flex`, text: `Flex 是 Flexible Box 的缩写,意为"弹性布局",用来为盒状模型提供最大的灵活性`, img: `19/08/flex.png`, link: ['http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html'] },
+  { info: 'web worker', title: `Web Worker`, text: `Web Worker 的作用,就是为 JavaScript 创造多线程环境,允许主线程创建 Worker 线程,将一些任务分配给后者运行`, img: `19/08/Web+Worker.png`, link: ['http://www.ruanyifeng.com/blog/2018/07/web-worker.html'] },
+
+
+  { info: '', title: `http`, text: `超文本传输协议是应用层上的一种B/S模型的通信协议,它由请求和响应构成,且是无状态的`, img: `19/08/http.png`, link: ['http://www.ruanyifeng.com/blog/2016/08/http.html', 'https://zhuanlan.zhihu.com/p/60450391?utm_source=wechat_session&utm_medium=social&utm_oi=625370033862348800'] },
+  { info: 'node', title: `Node`, text: `Node是JavaScript语言的服务器运行环境,首先JavaScript语言通过Node在服务器运行,其次Node提供大量工具库`, img: `19/08/Node.png`, link: ['http://javascript.ruanyifeng.com/nodejs/basic.html#'] },
+  { info: '', title: `前端性能监控`, text: `性能很重要,开发迭代过程中难免会有所忽视,性能会伴随产品的迭代而有所衰减;我们需要一套性能监控系统持续监控/评估/预警页面性能状况/发现瓶颈,指导优化工作的进行`, img: `19/08/前端性能监控.png`, link: ['https://www.cnblogs.com/dayiran1222/p/8745846.html'] },
+
+
+  { info: '', title: `正则表达式`, text: `正则表达式(regular expression)描述了一种字符串匹配的模式(pattern),可以用来检查一个串是否含有某种子串/将匹配的子串替换或者从某个串中取出符合某个条件的子串等`, img: `19/07/正则表达式.png` },
+  { info: '', title: `浏览器储存`, text: `浏览器存储常用的有cookie/localStorage/sessionStorage`, img: `19/07/浏览器储存.png` },
+  { info: '', title: `数据结构`, text: `数据结构是计算机存储/组织数据的方式;数据结构是指相互之间存在一种或多种特定关系的数据元素的集合;通常情况下,精心选择的数据结构可以带来更高的运行或者存储效率;数据结构往往同高效的检索算法和索引技术有关`, img: `19/07/数据结构.png` },
+  { info: '', title: `堆排序`, text: `堆排序(Heapsort)是指利用堆这种数据结构一种排序算法`, img: `19/07/堆排序.png` },
+
+  { info: '', title: `模块方案`, text: `网页越来越像桌面程序,需要一个团队分工协作/进度管理/单元测试等等;Javascript模块化编程,理想情况下,开发者只需要实现核心的业务逻辑,其他都可以加载别人已经写好的模块`, img: `19/07/模块方案.png` },
+  { info: '', title: `typescript`, text: `TypeScript 是微软2012年推出的一种编程语言,属于 JavaScript 的超集,可以编译为 JavaScript 执行; 它的最大特点就是支持强类型  `, img: `19/07/typescript.png` },
+  { info: '', title: `webpack`, text: `webpack 是一个现代 JavaScript 应用程序的静态模块打包器(module bundler);当 webpack 处理应用程序时,它会递归地构建一个依赖关系图(dependency graph),其中包含应用程序需要的每个模块,然后将所有这些模块打包成一个或多个 bundle`, img: `19/07/webpack.png` },
+
+
+  { info: '', title: `选择排序`, text: `第一次从待排序的数据元素中选出最小(或最大)的一个元素,存放在序列的起始位置,然后再从剩余的未排序元素中寻找到最小(大)元素,然后放到已排序的序列的末尾;以此类推,直到全部待排序的数据元素的个数为零(不稳定)`, img: `19/07/选择排序.png` },
+  { info: '', title: `插入排序`, text: `每步将一个待排序的记录,按其关键码值的大小插入前面已经排序的文件中适当位置上,直到全部插入完为止(稳定)`, img: `19/07/插入排序.png` },
+  { info: '', title: `冒泡排序`, text: `它重复地走访过要排序的元素列,依次比较两个相邻的元素,如果他们的顺序(如从大到小/首字母从A到Z)错误就把他们交换过来;走访元素的工作是重复地进行直到没有相邻元素需要交换,也就是说该元素列已经排序完成(稳定)`, img: `19/07/冒泡排序.png` },
+  { info: '', title: `js异步处理`, text: `在浏览器端,耗时很长的操作都应该异步执行`, img: `19/07/js异步处理.png` },
+
+  { info: '', title: `canvas`, text: `HTML5 <canvas> 元素用于图形的绘制,通过脚本 (通常是JavaScript)来完成. <canvas> 标签只是图形容器,您必须使用脚本来绘制图形. 你可以通过多种方法使用 canvas 绘制路径,盒/圆/字符以及添加图像.`, img: `19/07/canvas.png` },
+  { info: 'grid', title: `Grid`, text: `网格布局(Grid)是最强大的 CSS 布局方案;Flex 布局是轴线布局,只能指定"项目"针对轴线的位置,可以看作是一维布局;Grid 布局则是将容器划分成"行"和"列",产生单元格,然后指定"项目所在"的单元格,可以看作是二维布局`, img: `19/07/Grid.png` },
+  { info: '', title: `web 开发框架`, text: `koa2与express 提供的API大致相同,express是大而全,内置了大多数的中间件,更让人省心,koa2不绑定任何的框架,干净简洁,小而精,更容易实现定制化,扩展性好.express是没有提供ctx来提供上下流服务,需要更多的手动处理,express本身是不支持洋葱模型的数据流入流出能力的,需要引入其他的插件.`, img: `19/07/web+开发框架.png` },
+
+  { info: 'jwt', title: `JWT`, text: `JSON Web Token(JWT)是目前最流行的跨域认证解决方案`, img: `19/07/JWT.png` },
+  { info: 'restful api', title: `RESTful API`, text: `RESTful 是目前最流行的 API 设计规范,用于 Web 数据接口的设计`, img: `19/07/RESTful+API.png`, link: ['http://www.ruanyifeng.com/blog/2018/10/restful-api-best-practices.html'] },
+  { info: '', title: `proxy`, text: `Proxy 用于修改某些操作的默认行为,等同于在语言层面做出修改,所以属于一种“元编程”(meta programming),即对编程语言进行编程;Proxy 实际上重载(overload)了点运算符,即用自己的定义覆盖了语言的原始定义;Proxy 这个词的原意是代理,用在这里表示由它来“代理”某些操作,可以译为“代理器”;`, img: `19/07/proxy.png` },
+
+  { info: '', title: `常见网络攻击`, text: `常见网络攻击:XSS攻击全称跨站脚本攻击是一种在web应用中的计算机安全漏洞,它允许恶意web用户将代码植入到提供给其它用户使用的页面中.CSRF站请求伪造是一种对网站的恶意利用.CSRF通过伪装成受信任用户的请求来利用受信任的网站.CSRF攻击往往不大流行(因此对其进行防范的资源也相当稀少)和难以防范,所以被认为比XSS更具危险性`, img: `19/07/常见网络攻击.png` },
+  { info: '', title: `deno`, text: `Node 之父 Ryan Dahl 发布新的开源项目 deno,从官方介绍来看,可以认为它是下一代 Node,使用 Go 语言代替 C++ 重新编写跨平台底层内核驱动,上层仍然使用 V8 引擎,最终提供一个安全的 TypeScript 运行时`, img: `19/07/deno.png` },
+  { info: 'pwa', title: `PWA`, text: `PWA(Progressive web apps)运用现代的 Web API 以及传统的渐进式增强策略来创建跨平台 Web 应用程序`, img: `19/07/PWA.png` },
+]
