3228d3af1d43d962a8d6b5f621717cb896104ad1
jxj322991 <228322991@qq.com>
2019-12-25 13:16:11 +0800

    fix

---
+++ 2019/12/25/node.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @Date: 2019-12-25 13:11:57
+ * @LastEditTime : 2019-12-25 13:13:33
+ * @LastEditors  : 靳肖健
+ */
+var liveServer = require("live-server");
+
+var params = {
+  port: 8181, // Set the server port. Defaults to 8080.
+  host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+  root: "./", // Set root directory that's being served. Defaults to cwd.
+  open: false, // When false, it won't load your browser by default.
+  ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+  file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+  mount: [['/components', './node_modules']], // Mount a directory to a route.
+  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
