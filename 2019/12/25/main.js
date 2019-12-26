d5a3e8301afc5dc32e064e126ed9a5a92d6cd54c
jxj322991 <228322991@qq.com>
2019-12-26 13:36:39 +0800

    fix

--- 2019/12/25/main.js
+++ 2019/12/25/main.js
@@ -3,10 +3,43 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-12-25 13:05:32
- * @LastEditTime : 2019-12-25 13:36:17
+ * @LastEditTime : 2019-12-26 13:36:00
  * @LastEditors  : 靳肖健
  */
 !function () {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+  // 创建场景/摄像
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  // 创建画布
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+
+  //建立模型
+  var geometry = new THREE.BoxGeometry(2, 2, 2);
+  var material = new THREE.MeshBasicMaterial({ color: "#fff", wireframe: true });
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  //建立模型
+  var sphereGeometry = new THREE.SphereGeometry(1, 55, 55);
+  var material = new THREE.MeshBasicMaterial({ color: "#00f" });
+
+  var sphere = new THREE.Mesh(sphereGeometry, material);
+  scene.add(sphere);
+
+
+  camera.position.z = 5;
+
+  var animate = function () {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
 }()
--- 2019/12/25/node.js
+++ 2019/12/25/node.js
@@ -3,7 +3,7 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-12-25 13:11:57
- * @LastEditTime : 2019-12-25 13:26:01
+ * @LastEditTime : 2019-12-26 13:19:16
  * @LastEditors  : 靳肖健
  */
 var liveServer = require("live-server");
@@ -12,11 +12,5 @@ var params = {
   port: 8181, // Set the server port. Defaults to 8080.
   host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   root: "./", // Set root directory that's being served. Defaults to cwd.
-  open: true, // When false, it won't load your browser by default.
-  file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-  mount: [['/components', './node_modules']], // Mount a directory to a route.
-  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-  middleware: [function (req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
 };
 liveServer.start(params);
