533d247ef65d07bcee88e948eff691838be4e34b
jxj322991 <228322991@qq.com>
2019-12-30 14:08:15 +0800

    fix

--- 2019/12/25/js/main.js
+++ 2019/12/25/js/main.js
@@ -3,16 +3,17 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2019-12-25 13:05:32
- * @LastEditTime : 2019-12-26 13:37:49
+ * @LastEditTime : 2019-12-26 20:50:23
  * @LastEditors  : 靳肖健
  */
 !function () {
   // 创建场景/摄像
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+  // (视角, 长宽比,近界,远界)
+  var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100);
   // 创建画布
   var renderer = new THREE.WebGLRenderer();
+
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
@@ -24,7 +25,7 @@
   scene.add(cube);
 
   //建立模型
-  var sphereGeometry = new THREE.SphereGeometry(1, 55, 55);
+  var sphereGeometry = new THREE.SphereGeometry(0, 100, 100);
   var material = new THREE.MeshBasicMaterial({ color: "#00f" });
 
   var sphere = new THREE.Mesh(sphereGeometry, material);
@@ -37,7 +38,6 @@
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
   };
 
