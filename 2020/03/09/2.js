7ff39936954ad9ba5aba76803f6da9dbc9b001ef
jxj322991 <228322991@qq.com>
2020-03-09 18:22:52 +0800

    fix

--- 2020/03/09/2.js
+++ 2020/03/09/2.js
@@ -3,25 +3,58 @@
  * @Author: jinxiaojian
  * @Email: jinxiaojian@youxin.com
  * @Date: 2020-03-09 11:40:01
- * @LastEditTime: 2020-03-09 11:54:20
+ * @LastEditTime: 2020-03-09 14:24:24
  * @LastEditors: 靳肖健
  */
-function deep (node, nodeList) {
-  if (node) {
-    nodeList.push(node);
-    var children = node.children;
-    for (var i = 0; i < children.length; i++)
-      deep(children[i], nodeList);
+
+let tree = {
+  value: '1',
+  left: {
+    value: '2',
+    left: {
+      value: '3',
+    },
+    right: {
+      value: '4',
+      left: {
+        value: '5',
+      },
+      right: {
+        value: '6',
+      }
+    }
+  },
+  right: {
+    value: '7',
+    left: {
+      value: '8',
+    },
+    right: {
+      value: '9',
+    }
   }
-  return nodeList;
 }
 
-function range (node, nodeList) {
-  let i = 0;
-  if (!(node == null)) {
-    nodeList.push(node);
-    range(node.nextElementSibling, nodeList);
-    range(node.firstElementChild, nodeList);
+
+
+
+//深度
+function deep (node) {
+  let left = 0, right = 0
+  if (node.left) {
+    left = deep(node.left) + 1
+  } else if (node.right) {
+    right = deep(node.right) + 1
+  } else {
+    return 1
   }
-  return nodeList;
+  return left > right ? left : right
+}
+
+//广度
+function range (node) {
+  if (!node.left && !node.right) {
+    return 1
+  }
+  return range(node.left) + range(node.right)
 }
