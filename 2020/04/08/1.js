0386f4bbed4ffb806d52270aba5fcf1b2fff90ba
jxj322991 <228322991@qq.com>
2020-04-08 23:18:32 +0800

    fix

--- 2020/04/08/1.js
+++ 2020/04/08/1.js
@@ -1,5 +1,5 @@
 /*
- * @LastEditTime: 2020-04-08 00:19:39
+ * @LastEditTime: 2020-04-08 23:16:32
  * @LastEditors: jinxiaojian
  */
 
@@ -10,34 +10,89 @@
 // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 // 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-
-// 示例:
-
-// 给定 nums = [2, 7, 11, 15], target = 9
-
-// 因为 nums[0] + nums[1] = 2 + 7 = 9
-// 所以返回 [0, 1]
-
-// let twoSum = (nums, target) => {
-//   let targetMap = new Map()
-//   for (let i = 0; i < nums.length; i++) {
-//     const key = target - nums[i]
-//     if (targetMap.has(key)) {
-//       return [targetMap.get(key), i]
-//     }
-//     targetMap.set(nums[i], i)
-//   }
-// }
-// twoSum([5, 2, 11, 15, 7, 6], 9)
+let arr = [
+  2134,
+  234,
+  342,
+  3214,
+  234,
+  342,
+  3424,
+  23,
+  2344,
+  23,
+  2134,
+  324,
+  43,
+  564,
+  7,
+  214,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  8,
+  765,
+  234,
+  12,
+  342,
+  234,
+  214,
+  2341,
+  456,
+  87,
+  789,
+  87,
+  8,
+  78,
+  456,
+  2345,
+  123235,
+  23,
+  456,
+  689,
+  23523,
+  325345,
+];
+let tag = 3424 + 78;
 
 let twoSum = function(nums, target) {
-  for(let i=0;i<nums.length;i++){
-      let dif =target-nums[i]
-      for(let j=i+1;j<nums.length;j++){
-          if(nums[j]==dif){
-              return [i,j]
-          }
+  console.time();
+  for (let i = 0; i < nums.length; i++) {
+    let dif = target - nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] == dif) {
+        console.timeEnd();
+
+        return [i, j];
       }
+    }
   }
-  return new Error('未找到!')
-  };
+  return new Error("target不符合!");
+};
+twoSum(arr, tag);
+
+let twoSum2 = function(nums, target) {
+  console.time();
+  for (let i = 0; i < nums.length - 1; i++) {
+    let dif = target - nums[i];
+    let index = nums.indexOf(dif, i + 1);
+    if (index > 0) {
+      console.timeEnd();
+      return [i, index];
+    }
+  }
+  return new Error("target不符合!");
+};
+twoSum2(arr, tag);
