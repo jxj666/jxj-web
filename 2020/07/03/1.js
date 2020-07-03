0b46ba99e5cdf4a98cf01922e7b53076a8aa2556
jxj322991 <228322991@qq.com>
2020-07-03 11:19:42 +0800

    fix

---
+++ 2020/07/03/1.js
@@ -0,0 +1,61 @@
+/*
+ * @LastEditTime: 2020-07-03 11:19:24
+ * @LastEditors: jinxiaojian
+ */
+// /*
+//  * @LastEditTime: 2020-07-03 11:19:12
+//  * @LastEditors: jinxiaojian
+//  */ 
+// /*------count 的发布订阅者实践------*/
+// let state = {
+//   count: 1
+// };
+// let listeners = [];
+
+// /*订阅*/
+// function subscribe(listener) {
+//   listeners.push(listener);
+// }
+
+// function changeCount(count) {
+//   state.count = count;
+//   /*当 count 改变的时候，我们要去通知所有的订阅者*/
+//   for (let i = 0; i < listeners.length; i++) {
+//     const listener = listeners[i];
+//     listener();
+//   }
+// }
+
+// subscribe(() => {
+//   console.log(state.count);
+// });
+
+// changeCount(2);
+// changeCount(3);
+// changeCount(4);
+
+
+const creatStore = function (initState) {
+  let state = initState
+  let listeners = [];
+  /*订阅*/
+  function subscribe (listener) {
+    listeners.push(listener);
+  }
+  function changeState (newState) {
+    state = newState;
+    /*当 state 改变的时候，我们要去通知所有的订阅者*/
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i];
+      listener();
+    }
+  }
+  function getState () {
+    return state
+  }
+  return {
+    subscribe,
+    changeState,
+    getState
+  }
+}
