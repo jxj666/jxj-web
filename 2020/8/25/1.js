4efe31ea3a33e9a3a65fab0c34a60800e315a73b
jinxiaojian <jinxiaojian1992@gmail.com>
2020-08-25 10:12:20 +0800

    fix

---
+++ 2020/8/25/1.js
@@ -0,0 +1,37 @@
+/*
+ * @LastEditTime: 2020-08-25 10:11:59
+ * @LastEditors: jinxiaojian
+ */
+
+let list = [{
+  id: '1',
+  children: [{
+    id: '11',
+    children: [{
+      id: '111'
+    }, {
+      id: '112'
+    }]
+  }]
+}];
+const fn = (data, value) => {
+  let res = []
+  const dfs = (arr, temp = []) => {
+    for (const node of arr) {
+      if (node.children) {
+        dfs(node.children, temp.concat(node.id))
+      } else {
+        if (node.id == value) {
+          res = temp.concat(node.id)
+          return
+        }
+
+      }
+    }
+  }
+  dfs(data)
+  return res
+
+}
+
+fn(list, '112')
